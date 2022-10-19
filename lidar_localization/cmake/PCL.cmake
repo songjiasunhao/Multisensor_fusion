22d93d3457ed92c50cbdcf6f4d78a0f2d84231fa
sunhaonew <sunhao97@126.com>
2022-10-19 14:53:55 +0800

    第二章源码

--- lidar_localization/cmake/PCL.cmake
+++
@@ -1,7 +0,0 @@
-find_package(PCL  REQUIRED)
-list(REMOVE_ITEM PCL_LIBRARIES "vtkproj4")
-#APPEND　添加新element到list中
-#REMOVE_ITEM　从list中删除某个element
-include_directories(${PCL_INCLUDE_DIRS})
-list(APPEND ALL_TARGETS_LIBRARIES ${PCL_LIBRARIES})
-#把库的名字合并到这个变量中去
